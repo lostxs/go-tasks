b5ea4605ef9ec260c3f3dc573d239329af916c60
denis <denis.denis@test.com>
2025-09-28 01:36:21 +0300

    1'st task

---
+++ 1-converter/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+const (
+	UsdToEur = 0.95
+	UsdToRub = 94.20
+)
+
+func main() {
+	eurToRub := UsdToRub / UsdToEur
+
+	fmt.Println("Exchange rates")
+	fmt.Printf("1 USD = %.2f EUR\n", UsdToEur)
+	fmt.Printf("2 USD = %.2f RUB\n", UsdToRub)
+	fmt.Printf("3 EUR = %.2f RUB\n", eurToRub)
+}
