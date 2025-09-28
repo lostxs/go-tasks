346b7fb75c20d0607798850f17dbcbab61b35f00
denis <denis.denis@test.com>
2025-09-28 14:57:50 +0500

    second task

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -15,3 +15,11 @@ func main() {
 	fmt.Printf("2 USD = %.2f RUB\n", UsdToRub)
 	fmt.Printf("3 EUR = %.2f RUB\n", eurToRub)
 }
+
+func getUserInput() {
+	var val float64
+	fmt.Print("Enter value: ")
+	fmt.Scan(&val)
+}
+
+func calculateExchange(val int, original, target string) {}
