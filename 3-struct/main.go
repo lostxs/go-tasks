5090dd2f1860628c7c977672d254e85d8cab2a53
denis <denis.denis@test.com>
2025-10-01 23:40:15 +0500

    8 task

--- 3-struct/main.go
+++ 3-struct/main.go
@@ -1,48 +1,14 @@
 package main
 
 import (
+	"3-struct/bins"
 	"fmt"
-	"time"
 )
-
-type bin struct {
-	id        string
-	name      string
-	private   bool
-	createdAt time.Time
-}
-
-func newBin(id, name string, private bool) *bin {
-	return &bin{
-		id:        id,
-		name:      name,
-		private:   private,
-		createdAt: time.Now(),
-	}
-}
-
-type binList struct {
-	bins []bin
-}
-
-func (bl *binList) printBinList() {
-	fmt.Println(bl)
-}
-
-func (bl *binList) addBin(bin *bin) {
-	bl.bins = append(bl.bins, *bin)
-}
-
-func newBinList() *binList {
-	return &binList{
-		bins: []bin{},
-	}
-}
 
 func main() {
 	fmt.Println("Bins prog")
-	binList := newBinList()
-	bin := newBin("1", "first bin", false)
-	binList.addBin(bin)
-	binList.printBinList()
+	binList := bins.NewBinList()
+	bin := bins.NewBin("1", "first bin", false)
+	binList.AddBin(bin)
+	binList.PrintBinList()
 }
