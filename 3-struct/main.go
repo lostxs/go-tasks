aa6c068a9df48bbf4cd3f39d4329d518ac82c8ae
denis <denis.denis@test.com>
2025-10-04 13:37:28 +0500

    10 task

--- 3-struct/main.go
+++ 3-struct/main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"3-struct/bin"
+	"3-struct/files"
 	"3-struct/storage"
 	"fmt"
 )
 
 func main() {
-	storage := storage.NewStorage()
+	storage := storage.NewStorage(files.NewJsonDb("bins.json"))
 	bin := bin.NewBin("2", "new bin 2", true)
-	storage.AddBin(*bin)
+	storage.AddBin(bin)
 	bins := storage.GetBins()
 	fmt.Println(bins)
 }
