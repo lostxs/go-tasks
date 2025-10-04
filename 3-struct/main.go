774eaf6bbe3acfd55029f9f7615be132d47a3556
denis <denis.denis@test.com>
2025-10-05 02:39:16 +0500

    12 tasks

--- 3-struct/main.go
+++ 3-struct/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"3-struct/api"
 	"3-struct/bin"
+	"3-struct/config"
 	"3-struct/files"
 	"3-struct/storage"
 	"fmt"
 )
 
 func main() {
+	cfg := config.NewConfig()
+	api := api.NewApi(cfg)
+	fmt.Printf("API KEY: %s", api.Key)
 	storage := storage.NewStorage(files.NewJsonDb("bins.json"))
 	bin := bin.NewBin("2", "new bin 2", true)
 	storage.AddBin(bin)
